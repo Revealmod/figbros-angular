14f9e4f3aec279779b89946a1a623b6a3656915c
Juan de Urtubey <jdeurt@gmail.com>
2019-01-07 19:36:32 +0100

    Added better logging.

--- src/assets/js/functions.js
+++ src/assets/js/functions.js
@@ -4,9 +4,11 @@ function handleLogin() {
         password: $("#login-password").text()
     }, (data) => {
         if (data.success) {
-            console.log("Login succeeded! Multipass URL: " + data.url);
+            console.log("Login succeeded! Multipass URL: ");
+            console.log(data.url);
         } else {
-            console.log("Login failed with reason: " + data.info);
+            console.log("Login failed with reason: ");
+            console.log(data.info);
         }
     }, "json");
 
