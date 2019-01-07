9fb060d65c06e3c21fa5753f4a4c906c5cd8b7ac
Juan de Urtubey <jdeurt@gmail.com>
2019-01-07 19:07:34 +0100

    Added login form functions.

---
+++ src/assets/js/functions.js
@@ -0,0 +1,14 @@
+function handleLogin() {
+    $.post("http://api.sh.dev.figbros.com", {
+        username: $("#login-username").text(),
+        password: $("#login-password").text()
+    }, (data) => {
+        if (data.success) {
+            console.log("Login succeeded! Multipass URL: " + data.url);
+        } else {
+            console.log("Login failed with reason: " + data.info);
+        }
+    }, "json");
+
+    return false;
+}
